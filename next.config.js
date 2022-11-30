14f5ed69b0a630ff929c2e6ec4df71e0bec83654
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2022-11-30 21:53:15 +0530

    Test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
+  },
+  images: {
+    loader: 'custom',
+    loaderFile: './loader.js',
   },
 };
 
